f1427fe193a2d984ebaf0c08c5872a7bf148b5a1
pranithaparasa25 <parasapranitha25@gmail.com>
2025-09-19 17:44:02 +0530

    React Component State

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='border border-2 border-dark p-3 m-2'>
+      <h1>React Component</h1>
+    </div>
+  );
+}
+
+export default App;
