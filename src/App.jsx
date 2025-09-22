ddf09659a6455394aac98cb085b51ee8ac657b77
pranithaparasa25 <parasapranitha25@gmail.com>
2025-09-22 14:20:11 +0530

    counter-props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Counter from './Counter1';
 
 function App() {
   return (
     <div className='border border-2 border-dark p-3 m-2'>
       <h1>React Component</h1>
+      <Counter></Counter>
     </div>
   );
 }
